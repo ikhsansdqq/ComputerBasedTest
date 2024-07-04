b31533a85ae840b0581c3f5f918813f8cf93a6cf
Ikhsan Assidiqie <ikhsansdqq@student.telkomuniversit.ac.id>
2024-07-04 21:50:05 +0700

    Fixing Navbar & Revising Page

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,12 +1,21 @@
+"use client"
+
 import Image from 'next/image'
 import React from 'react'
 import Logo from '../../assets/images/Logo.png'
+
 const Navbar = () => {
   return (
     <div className='flex justify-between mx-[240px] items-center py-8'>
-        <Image src={Logo} height={48} width={172}/>
-        <p>Ikhsan Assidiqie (1301213047)</p>
-        <button className='border rounded-sm px-4 py-2'>Exit</button>
+      <Image
+        src={Logo}
+        height={48}
+        width={172}
+        onContextMenu={(e) => e.preventDefault()}
+        draggable="false"
+      />
+      <p>Ikhsan Assidiqie (1301213047)</p>
+      <button className='border rounded-sm px-4 py-2'>Exit</button>
     </div>
   )
 }
