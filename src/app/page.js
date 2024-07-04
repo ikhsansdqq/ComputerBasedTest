a537242b0312ce02f071e52ee0e51eecead82190
Dio Siregar <dio.siregar03@gmail.com>
2024-07-04 20:17:00 +0700

    feat/proctoring

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,12 @@
 import Head from 'next/head';
 import ExamMonitoring from '../components/ExamMonitoring';
+import Navbar from '@/components/Navbar';
 
 export default function Home() {
   return (
-    <div>
-      <Head>
-        <title>Exam Monitoring with TensorFlow.js</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1>Exam Monitoring using TensorFlow.js in Next.js</h1>
-        <ExamMonitoring />
-      </main>
-    </div>
+    <>
+      <Navbar/>
+      <ExamMonitoring/>
+    </>
   );
 }
