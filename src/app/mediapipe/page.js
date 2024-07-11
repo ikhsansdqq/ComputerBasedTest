3cb2509c362621f246244024f57c19c2f874ff55
ikhsansdqq <ikhsansdqq@student.telkomuniversity.ac.id>
2024-07-11 22:27:49 +0700

    Add Video Testing

---
+++ src/app/mediapipe/page.js
@@ -0,0 +1,10 @@
+import FaceDetectionPage from "@/components/FaceDetection";
+
+export default function MediaPipePage() {
+     return (
+          <div>
+               <h1>Au Ah Capek</h1>
+               <FaceDetectionPage />
+          </div>
+     )
+}
