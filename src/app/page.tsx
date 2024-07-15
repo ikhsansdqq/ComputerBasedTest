c4fbadd5e39e3812ff9291fb5bd667c5caadeaea
ikhsansdqq <ikhsansdq@student.telkomuniversity.ac.id>
2024-07-15 09:51:28 +0700

    Fixing Root layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ const HomePage = () => {
             How would you like to use Computer Based Test?
           </h1>
           <p className="mt-2 text-center text-sm text-gray-600">
-            Anyhow, we're here to help you out what's right for you 😎
+            Anyhow, we&apos;re here to help you out what&apos;s right for you 😎
           </p>
         </div>
         <div className="flex flex-col space-y-4">
@@ -20,26 +20,26 @@ const HomePage = () => {
                 <div>
                   <h3 className="text-lg font-semibold text-gray-900">Typescript Page</h3>
                   <p className="text-sm text-gray-600">
-                    Let's start collaborating with your teammates and unlock the full access of Dosehost. Hope you'll enjoy.
+                    Let&apos;s start collaborating with your teammates and unlock the full access of Dosehost. Hope you&apos;ll enjoy.
                   </p>
                 </div>
               </div>
             </div>
           </Link>
-          <Link href="/js">
+          {/* <Link href="/js">
             <div className="group border rounded-lg p-6 bg-white shadow hover:shadow-lg transform hover:scale-105 transition-all duration-200 ease-in-out cursor-pointer">
               <div className="flex items-center space-x-4">
                 <img src="/js_logo.png" alt="Javascript Page" className="h-12 w-12 rounded" />
                 <div>
                   <h3 className="text-lg font-semibold text-gray-900">Javascript Page</h3>
                   <p className="text-sm text-gray-600">
-                    It's a best option for you to explore or do something for all by yourself. Later, you can create a team workspace, that's easy.
+                    It&apos;s a best option for you to explore or do something for all by yourself. Later, you can create a team workspace, that's easy.
                   </p>
                   <span className='text-sm font-normal text-gray-600 mt-4'>(On Progress)</span>
                 </div>
               </div>
             </div>
-          </Link>
+          </Link> */}
         </div>
         {/* <div className="flex justify-center mt-6">
           <button className="bg-green-500 text-white px-6 py-2 rounded-full hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">
