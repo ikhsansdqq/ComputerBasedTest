3d7a249bb9ea7a7ff652780d268225bbb3dc26de
Ikhsan Assidiqie <ikhsansdqq@student.telkomuniversity.ac.id>
2024-11-04 07:33:07 +0700

    Fix bugs

--- src/app/exam/page.js
+++ src/app/exam/page.js
@@ -1,13 +1,22 @@
+"use client";
+
 import dynamic from "next/dynamic";
+import { useEffect, useState } from "react";
 
 const ClientOnlyComponent = dynamic(() => import("@/components/ClientOnlyComponent"), {
-  ssr: true,
+  ssr: false,
 });
 
 const ScriptPage = () => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   return (
     <div>
-      <ClientOnlyComponent />;
+      {isClient && <ClientOnlyComponent />}
     </div>
   )
 };
